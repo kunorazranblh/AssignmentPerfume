93b212b21c46052b0611f988fa241e92a9f6fcfb
USER <USER@192.168.8.101>
2020-12-20 15:57:28 +0800

    Perfume

---
+++ assignment1/src/assignment1/Perfume.java
@@ -0,0 +1,29 @@
+package assignment1;
+
+public class Perfume{
+	
+String ForMan, ForWoman;
+
+public Perfume(String ForMan, String ForWoman){
+this.ForMan = ForMan;
+this.ForWoman = ForWoman;
+}
+
+public void setForMan(String ForMan){
+this.ForMan = ForMan;
+}
+
+public void setForWoman(String ForWoman){
+this.ForWoman= ForWoman;
+}
+
+
+public String getForMan(){
+return ForMan;
+}
+
+public String getForWoman(){
+return ForWoman;
+}
+
+}
---
+++ assignment1/src/assignment1/mainPerfume.java
@@ -0,0 +1,35 @@
+package assignment1;
+
+public class mainPerfume{
+	
+	public static void main(String[] args) {
+
+Perfume f = new Perfume("For Him", "For Her");
+
+PerfumeBrand b = new PerfumeBrand ("DKNY", "CK", "Victoria Secret", "Givenchy", "Bulgari", "Hugo Boss");
+
+System.out.println("Type of Perfume: " + f.getForMan());
+System.out.println("Brand perfume for Man: ");
+System.out.println(b.getBrand1());
+
+System.out.println(b.getBrand5());
+
+System.out.println(b.getBrand6());
+System.out.println("");
+
+System.out.println("Type of Makeup: " + f.getForWoman());
+System.out.println("Brand perfume for Woman: ");
+System.out.println(b.getBrand1());
+
+System.out.println(b.getBrand2());
+
+System.out.println(b.getBrand3());
+
+System.out.println(b.getBrand4());
+
+System.out.println(b.getBrand5());
+System.out.println("");
+
+	}
+}
+
